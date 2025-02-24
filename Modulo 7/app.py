ff7aa1c978d73c4607d56085e4434fbede8bb425
ceciorosco <corosco@gmail.com>
2025-02-24 16:50:00 +0100

    actualizacion 24022025 1649

---
+++ Modulo 7/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# API reference de elementos de texto en Streamlit
+
+st.title('Elementos de texto en Streamlit')
+st.header('Esto es un encabezado')
+st.subheader('Esto es un sub-encabezado')
+
+st.write('Esto es un textyo normal, con la funcion write (**admite formateo**)')
+st.text('Esto es un texto normal, conj la funcion text (sin formato)')
+
+
