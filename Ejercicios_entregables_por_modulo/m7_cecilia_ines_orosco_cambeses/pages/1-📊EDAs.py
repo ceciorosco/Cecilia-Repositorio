0eadf426a1032d0c230f441fb79f188f6d111b7a
ceciorosco <corosco@gmail.com>
2025-03-02 12:25:24 +0100

    actualizacion 02032025 1225

--- Ejercicios_entregables_por_modulo/m7_cecilia_ines_orosco_cambeses/pages/1-📊EDAs.py
+++ Ejercicios_entregables_por_modulo/m7_cecilia_ines_orosco_cambeses/pages/1-📊EDAs.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -26,40 +28,46 @@ st.header('1. Carga de Datos')
 df = sns.load_dataset('diamonds').dropna()
 st.title('Gráficos de Matplotlib, Seaborn y Plotly en Streamlit')
 st.dataframe(df.head(5))
+st.divider()
+
 
 # Gráficos univariantes
 
 st.header('2. Gráficos univariantes')
 
-#filtros
-cut_filter= st.multiselect('Introduce corte (cut)', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'], default=df['cut'].unique())
-df_filtrado = df[df['cut'].isin(cut_filter)]
-
 columna = st.selectbox("Selecciona la columna numérica", options=df.select_dtypes('number').columns)
-fig, ax = plt.subplots(figsize=(6,4))
-sns.kdeplot(df_filtrado, x=columna)
-sns.rugplot(df_filtrado, x=columna)
-sns.histplot(df_filtrado, x=columna, kde=True)
-ax.set_title(f'Análisis univariante de {columna}')
-ax.set_xlabel(f'{columna}')
-ax.set_ylabel('Frecuencia')
-st.pyplot(fig)
 
-fig, ax = plt.subplots(figsize=(6,4))
-dato = df_filtrado[columna]
-ax.boxplot(dato, showmeans=True)
-ax.set_title(f'Boxplot de {columna}')
-ax.set_xlabel(f'{columna}')
-ax.set_ylabel('Frecuencia')
-st.pyplot(fig)
+tab00, tab01 = st.tabs(['2.1 Histograma', '2.2 Boxplot'])
+
+with tab00:
+    fig, ax = plt.subplots(figsize=(6,4))
+    sns.kdeplot(df, x=columna)
+    sns.rugplot(df, x=columna)
+    sns.histplot(df, x=columna, kde=True)
+    ax.set_title(f'Análisis univariante de {columna}')
+    ax.set_xlabel(f'{columna}')
+    ax.set_ylabel('Frecuencia')
+    st.pyplot(fig)
+
+with tab01:
+    fig, ax = plt.subplots(figsize=(6,4))
+    dato = df[columna]
+    ax.boxplot(dato, showmeans=True)
+    ax.set_title(f'Boxplot de {columna}')
+    ax.set_xlabel(f'{columna}')
+    ax.set_ylabel('Frecuencia')
+    st.pyplot(fig)
+st.divider()
 
 st.header('3. Gráficos bivariantes')
 
 #filtros
 columna_x = st.selectbox("Selecciona variable X", options=df.columns, key='x')
 columna_y = st.selectbox("Selecciona variable Y", options=df.columns, key='y')
-fig_scatter = px.scatter(df_filtrado, x=columna_x, y=columna_y, title=f'Scatter plot de {columna_x} vs {columna_y}')
+fig_scatter = px.scatter(df, x=columna_x, y=columna_y, title=f'Scatter plot de {columna_x} vs {columna_y}')
 st.plotly_chart(fig_scatter)
+
+st.divider()
 
 st.header('4. Gráficos multivariantes')
 
@@ -68,110 +76,121 @@ columna_x = st.selectbox("Selecciona variable X", options=df.columns, key='o')
 columna_y = st.selectbox("Selecciona variable Y", options=df.columns, key='p')
 columna_z = st.selectbox("Selecciona variable Z", options=df.columns, key='q')
 
-st.subheader('4.1 Seaborn')
+tab1, tab2, tab3, tab4 = st.tabs(['4.1 Seaborn', '4.2. Plotly', '4.3 Heatmap numéricas', '4.4 Pairplot'])
 
-fig, ax = plt.subplots(figsize=(6,4))
-sns.scatterplot(df, x=columna_x, y=columna_y, hue=columna_z, palette='autumn', ax=ax)
-ax.set_title(f'Relación entre {columna_x}, {columna_y} y {columna_z} ')
-ax.set_xlabel(f'{columna_x}')
-ax.set_ylabel(f'{columna_y}')
-ax.legend()
-ax.grid()
-st.pyplot(fig)
+with tab1:
+    st.subheader('4.1 Seaborn')
 
-st.subheader('4.2. Plotly')
+    fig, ax = plt.subplots(figsize=(6,4))
+    sns.scatterplot(df, x=columna_x, y=columna_y, hue=columna_z, palette='autumn', ax=ax)
+    ax.set_title(f'Relación entre {columna_x}, {columna_y} y {columna_z} ')
+    ax.set_xlabel(f'{columna_x}')
+    ax.set_ylabel(f'{columna_y}')
+    ax.legend()
+    ax.grid()
+    st.pyplot(fig)
 
-fig = px.scatter(df, x=columna_x, y=columna_y, color=columna_z, color_continuous_scale='viridis')
-st.plotly_chart(fig)
+with tab2:
+    st.subheader('4.2. Plotly')
 
-st.subheader('4.3 Heatmap numéricas')
+    fig = px.scatter(df, x=columna_x, y=columna_y, color=columna_z, color_continuous_scale='viridis')
+    st.plotly_chart(fig)
 
-fig, ax = plt.subplots(figsize=(6,4))
-sns.heatmap(df.corr(numeric_only=True).round(2), annot=True)
-ax.set_title('Heatmap de correlación numéricas')
-st.pyplot(fig)
+with tab3:
+    st.subheader('4.3 Heatmap numéricas')
+
+    fig, ax = plt.subplots(figsize=(6,4))
+    sns.heatmap(df.corr(numeric_only=True).round(2), annot=True)
+    ax.set_title('Heatmap de correlación numéricas')
+    st.pyplot(fig)
+    st.divider()
+    
+with tab4:
+    st.subheader('4.4 Pairplot')
+    variables = ['x', 'y', 'z']
+    grid = sns.pairplot(df, vars=variables, hue='cut')
+    st.pyplot(grid.fig)
 
 st.header('5. Gráficos con todos los filtros globales')
 
 st.subheader('5.1 Filtros')
 
-st.write('5.1.a. Filtros categóricos')
+with st.expander('5.1.a. Filtros categóricos'):
 
-col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)
 
-color = df['color'].unique().tolist()
-with col1:
-    selected_color = st.multiselect('Selecciona uno o varios colores', options=color, default=color)
+    color = df['color'].unique().tolist()
+    with col1:
+        selected_color = st.multiselect('Selecciona uno o varios colores', options=color, default=color)
 
-clarity = df['clarity'].unique().tolist()
-with col2:
-    selected_clarity = st.multiselect('Selecciona una o varias claridades', options=clarity, default=clarity)
+    clarity = df['clarity'].unique().tolist()
+    with col2:
+        selected_clarity = st.multiselect('Selecciona una o varias claridades', options=clarity, default=clarity)
 
-cut = df['cut'].unique().tolist()
-with col3:
-    selected_cut = st.multiselect('Selecciona uno o varios cortes', options=cut, default=cut)
+    cut = df['cut'].unique().tolist()
+    with col3:
+        selected_cut = st.multiselect('Selecciona uno o varios cortes', options=cut, default=cut)
 
-st.write('5.1.b. Filtros numéricos')
+with st.expander('5.1.b. Filtros numéricos'):
 
-# obtenemos mínimo y máximo para usarlo en los filtros de abajo
-col4, col5, col6 = st.columns(3)
+    col4, col5, col6 = st.columns(3)
 
-with col4:
-    carat_min, carat_max = st.slider(
-        'Selecciona rango de peso (carat)', 
-        min_value=df['carat'].min(),
-        max_value=df['carat'].max(),
-        value=(df['carat'].min(), df['carat'].max())
+    with col4:
+        carat_min, carat_max = st.slider(
+            'Selecciona rango de peso (carat)', 
+            min_value=df['carat'].min(),
+            max_value=df['carat'].max(),
+            value=(df['carat'].min(), df['carat'].max())
+        )
+
+    with col5:
+        depth_min, depth_max = st.slider(
+            'Selecciona rango de densidad (depth)', 
+            min_value=df['depth'].min(),
+            max_value=df['depth'].max(),
+            value=(df['depth'].min(), df['depth'].max())
+        )
+
+    with col6:
+        table_min, table_max = st.slider(
+            'Selecciona rango de mesa (table)', 
+            min_value=df['table'].min(),
+            max_value=df['table'].max(),
+            value=(df['table'].min(), df['table'].max())
+        )
+
+    price_min, price_max = st.slider(
+        'Selecciona rango de precio (price)', 
+        min_value=df['price'].min(),
+        max_value=df['price'].max(),
+        value=(df['price'].min(), df['price'].max())
     )
 
-with col5:
-    depth_min, depth_max = st.slider(
-        'Selecciona rango de densidad (depth)', 
-        min_value=df['depth'].min(),
-        max_value=df['depth'].max(),
-        value=(df['depth'].min(), df['depth'].max())
-    )
+    col7, col8, col9 = st.columns(3)
 
-with col6:
-    table_min, table_max = st.slider(
-        'Selecciona rango de mesa (table)', 
-        min_value=df['table'].min(),
-        max_value=df['table'].max(),
-        value=(df['table'].min(), df['table'].max())
-    )
+    with col7:
+        x_min, x_max = st.slider(
+            'Selecciona rango de alto (x)', 
+            min_value=df['x'].min(),
+            max_value=df['x'].max(),
+            value=(df['x'].min(), df['x'].max())
+        )
 
-price_min, price_max = st.slider(
-    'Selecciona rango de precio (price)', 
-    min_value=df['price'].min(),
-    max_value=df['price'].max(),
-    value=(df['price'].min(), df['price'].max())
-)
+    with col8:
+        y_min, y_max = st.slider(
+            'Selecciona rango de ancho (y)', 
+            min_value=df['y'].min(),
+            max_value=df['y'].max(),
+            value=(df['y'].min(), df['y'].max())
+        )
 
-col7, col8, col9 = st.columns(3)
-
-with col7:
-    x_min, x_max = st.slider(
-        'Selecciona rango de alto (x)', 
-        min_value=df['x'].min(),
-        max_value=df['x'].max(),
-        value=(df['x'].min(), df['x'].max())
-    )
-
-with col8:
-    y_min, y_max = st.slider(
-        'Selecciona rango de ancho (y)', 
-        min_value=df['y'].min(),
-        max_value=df['y'].max(),
-        value=(df['y'].min(), df['y'].max())
-    )
-
-with col9:
-    z_min, z_max = st.slider(
-        'Selecciona rango de profundidad (z)', 
-        min_value=df['z'].min(),
-        max_value=df['z'].max(),
-        value=(df['z'].min(), df['z'].max())
-    )
+    with col9:
+        z_min, z_max = st.slider(
+            'Selecciona rango de profundidad (z)', 
+            min_value=df['z'].min(),
+            max_value=df['z'].max(),
+            value=(df['z'].min(), df['z'].max())
+        )
 
 # Aplicar los filtros
 df_filtered = df[
@@ -195,5 +214,64 @@ st.write(f'Nº filas eliminadas por filtro: **{df.shape[0] - df_filtered.shape[0
 
 
 st.subheader('5.2 Gráficos')
+
+
+
+st.write('Gráficos nativos de Streamlit')
+
+with st.expander('Gráfica de barras: st.bar_chart 📊'):
+    st.subheader('Gráfica de barras: st.bar_chart 📊')
+    df_group_by_cut = df_filtered.groupby('cut', observed=True)['price'].sum()
+    st.table(df_group_by_cut)
+    st.bar_chart(df_group_by_cut, color=['#00ff00'])
+    #st.bar_chart(area_data)
+
+with st.expander('Gráfica de líneas: st.line_chart'):
+    
+    line_data = df_filtered.sort_values('carat')[['carat', 'price']].set_index('carat')
+    st.line_chart(line_data)
+    
+with st.expander('Gráfico de área: st.area_chart'):
+    area_data = df_filtered.groupby('cut')['price'].mean().reset_index()
+    area_data.set_index('cut', inplace=True)
+    st.area_chart(area_data)
+
+# with st.expander('Gráfico de mapa: st.map'):
+#     num_points = df_filtered.shape[0]
+#     map_data = pd.DataFrame({
+#         'lat': np.random.uniform(37, 38, size=num_points),
+#         'lon': np.random.uniform(-122, -121, size=num_points)
+#     })
+#     st.map(map_data)
+
+with st.expander('Gráfico de scatter: st.scatter_chart'):
+    scatter_data = df[['carat', 'price']]
+    st.scatter_chart(scatter_data)
+
+st.divider()
+
+
+#Descargar datos
+st.header('6. Descargar datos')
+st.html('<p style="text-align:center">Descarga el dataset original o con los filtros actuales</p>')
+
+col1, col2 = st.columns(2, vertical_alignment='center')
+
+with col1:
+    st.download_button(
+        'Descargar datos originales',
+        data=df.to_csv(index=False),
+        file_name='diamonds_original.csv',
+        mime='text/csv'
+    ) 
+    
+with col2:    
+    st.download_button(
+        'Descargar datos filtrados',
+        data=df_filtered.to_csv(index=False),
+        file_name='diamonds_filtered.csv',
+        mime='text/csv'
+    )
+    
 
 
--- Ejercicios_entregables_por_modulo/m7_cecilia_ines_orosco_cambeses/pages/3-🧬Clasificacion.py
+++ Ejercicios_entregables_por_modulo/m7_cecilia_ines_orosco_cambeses/pages/3-🧬Clasificacion.py
@@ -3,7 +3,7 @@ import joblib
 import seaborn as sns
 import pandas as pd
 
-st.set_page_config(page_title='2-🧬Clasificación', page_icon=':dna:')
+st.set_page_config(page_title='3-🧬Clasificación', page_icon=':dna:')
 st.title('Página 3 - Clasificación')
 
 col1, col2, col3 = st.columns(3)
@@ -39,56 +39,63 @@ st.header('Introduce los siguientes datos para la predicción:')
 with st.form('mi_formulario'):
     carat = st.number_input(
         'Introduce peso (carat):',
+        value=df['carat'].mean(),
         min_value=0.20,
         max_value=5.01,
-        value=df['depth'].mode()[0],
         step=0.01
     )
+    color = st.selectbox('Introduce color (color)', df['color'].unique().tolist())
+    
+    clarity = st.selectbox('Introduce claridad (clarity)', df['clarity'].unique().tolist())
+    
     depth = st.number_input(
         'Introduce profundidad (depth):',
-        min_value=43,
-        max_value=79,
-        value=df['depth'].mode()[0],
-        step=0.1
+        value=df['depth'].mean(),
+        min_value=df['depth'].min(), 
+        max_value=df['depth'].max(),
+        step=0.05
     )
+    
     price = st.number_input(
         'Introduce precio (price):',
-        min_value=326.00,
-        max_value=18823.00,
-        value=df['price'].mode()[0],
+        value=int(df['price'].mode()),
+        min_value=df['price'].min(), 
+        max_value=df['price'].max(),
         step=1
     )
+    
     table = st.number_input(
         'Introduce mesa (table):',
-        min_value=43,
-        max_value=95,
-        value=df['table'].mode()[0],
-        step=1
+        value=df['table'].mean(),
+        min_value=df['table'].min(), 
+        max_value=df['table'].max(),
+        step=0.1
     )
+    
     x = st.number_input(
         'Introduce largo mm (x):',
-        min_value=0.01,
-        max_value=10.70,
-        value=df['x'].mode()[0],
+        value=df['x'].mean(),
+        min_value=df['x'].min(), 
+        max_value=df['x'].max(),
         step=0.01
     )
+    
     y = st.number_input(
         'Introduce ancho mm (y):',
-        min_value=0.01,
-        max_value=58.90,
-        value=df['y'].mode()[0],
+        value=df['y'].mean(),
+        min_value=df['y'].min(), 
+        max_value=df['y'].max(),
         step=0.01
     )
+    
     z = st.number_input(
         'Introduce profundidad mm (z):',
-        min_value=0.01,
-        max_value=31.80,
-        value=df['z'].mode()[0],
+        value=df['z'].mean(),
+        min_value=df['z'].min(),
+        max_value=df['z'].max(),
         step=0.01
     )
-    color = st.selectbox('Introduce color (color)', ['D', 'E', 'F', 'G', 'H', 'I', 'J'])
-    clarity = st.selectbox('Introduce claridad (clarity)', ['SI2', 'SI1', 'VS1', 'VS2', 'VVS2', 'VVS1', 'I1', 'IF'])
-    
+     
     boton_enviar = st.form_submit_button('Generar predicción')
 
     if boton_enviar:
@@ -99,18 +106,23 @@ with st.form('mi_formulario'):
             'depth': [depth],
             'table': [table],
             'price': [price],
-            'x':['x'],
-            'y':['z'],
-            'z':['z']
+            'x':[x],
+            'y':[z],
+            'z':[z],
         })
-        prediccion = model.predict(X_new)[0]
+        prediccion = model.predict(X_new)
+        proba = model.predict_proba(X_new).max() * 100
+
+        # Convertir el valor numérico a etiqueta usando el diccionario
+        cut_dic = {0: 'Fair',
+           1: 'Good',
+           2: 'Ideal',
+           3: 'Premium',
+           4: 'Very good'}
+        
+        corte_estimado = cut_dic[prediccion[0]]
+
         col1, col2 = st.columns(2)
-        col1.metric('Corte estimado (prediccion): ', value=f'{prediccion}', delta=f'{cut_mode}')
-        col2.metric('Corte promedio: ', value=f'{cut_mode} €')
-
-
-
-
-
-
+        col1.metric('Corte estimado (prediccion): ', value=corte_estimado)
+        col2.metric('Corte promedio: ', value=f'{proba:.2f} %')
 
